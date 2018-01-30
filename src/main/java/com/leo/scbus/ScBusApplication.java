df30cb72ef91d04179af5e16dd69f0d1549ea490
wangly1004 <wangliyingwad@163.com>
2018-01-30 21:45:46 +0800

    1.bus initial

---
+++ src/main/java/com/leo/scbus/ScBusApplication.java
@@ -0,0 +1,12 @@
+package com.leo.scbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScBusApplication.class, args);
+	}
+}
